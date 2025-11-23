ebe0ff923c4e8aefd0b54f11d44ec5b980c73bab
Khoi123345 <hoquockhoi.114@gmail.com>
2025-11-23 14:38:53 +0700

    fix: add RedisTemplate bean to RedisConfig

--- foodfast-delivery/services/drone-service/src/main/java/com/programming/droneservice/config/RedisConfig.java
+++ foodfast-delivery/services/drone-service/src/main/java/com/programming/droneservice/config/RedisConfig.java
@@ -4,11 +4,24 @@ import com.programming.droneservice.listener.DroneEventListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        return template;
+    }
     
     @Bean
     public RedisMessageListenerContainer container(
